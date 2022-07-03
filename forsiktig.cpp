04ec7e6bfa5270be07063ef0c99105f0188d61c1
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-07-03 20:01:08 +0200

    Update forsiktig.cpp
    Hate that spam

--- forsiktig.cpp
+++ forsiktig.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[]) {
     while (!_kbhit()) {
         std::cout << "Press any key to start the game..." << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
 
     std::cout << "- 'q' to quit\n";
