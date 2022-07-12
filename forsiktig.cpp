469876bcaad2895fe3660c60476350adb7a5fded
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-07-12 14:06:51 +0200

    Improved CL arguments

--- forsiktig.cpp
+++ forsiktig.cpp
@@ -1,18 +1,26 @@
 #include "game.hpp"
 // g++ forsiktig.cpp -o forsiktig.exe
 
+#define VERSION "1.0.0"
+#define DATE "12-07-2022"
+
 
 int main(int argc, char* argv[]) {
     if (argc == 2) {
         if (strcmp(argv[1], "--source") == 0) {
             std::cout << "https://github.com/Lioydiano/Forsiktig" << std::endl;
+        } else if (strcmp(argv[1], "--version") == 0) {
+            std::cout << "Forsiktig " << VERSION << " (" << DATE << ")" << std::endl;
+        } else {
+            std::cout << "Forsiktig " << VERSION << " (" << DATE << ")" << std::endl;
+            std::cout << "Usage: " << argv[0] << " [--source | --version]" << std::endl;
         }
         return 0;
     }
 
     CLS;
     std::cout << "\x1b]2;Forsiktig\x07"; // Set window title
-    std::cout << "Welcome to Forsiktig!\nFLAK-ZOSO 3/7/22\n\n";
+    std::cout << "\x1b[31;1mForsiktig v" << VERSION << "\nFLAK-ZOSO " << DATE << "\x1b[0m\n\n";
     std::cout << "Instructions:\n";
     std::cout << "- 'q' to quit\n";
     std::cout << "- 'p' to pause\n";
