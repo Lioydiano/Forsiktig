f1a868677f0884a5943b9c14bb81d3bdc76f9287
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-08-16 08:31:48 +0200

    Stable release

--- forsiktig.cpp
+++ forsiktig.cpp
@@ -1,8 +1,8 @@
 #include "game.hpp"
 // g++ forsiktig.cpp -o forsiktig.exe
 
-#define VERSION "1.5.0b1"
-#define DATE "15-08-2022"
+#define VERSION "1.5.0"
+#define DATE "16-08-2022"
 
 
 int main(int argc, char* argv[]) {
