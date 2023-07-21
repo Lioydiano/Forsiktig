d5bcf6f1d85c838f06cc0877c78a09b78ec370ee
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2023-07-21 16:35:23 +0200

    Forsiktig - intro screen

--- forsiktig.cpp
+++ forsiktig.cpp
@@ -1,8 +1,8 @@
 #include "game.hpp"
 // g++ forsiktig.cpp -o forsiktig.exe
 
-#define VERSION "1.9.1"
-#define DATE "11-7-2023"
+#define VERSION "1.9.2"
+#define DATE "21-07-2023"
 
 
 int main(int argc, char* argv[]) {
@@ -27,7 +27,11 @@ int main(int argc, char* argv[]) {
     std::cout << "\t\t\t- '\x1b[35mp\x1b[0m' to \x1b[3mpause\x1b[0m\n";
     std::cout << "\t\t\t- '\x1b[35mr\x1b[0m' to \x1b[3mresume\x1b[0m <-- \x1b[3mstart\x1b[0m the game\n";
     std::cout << "\t\t\t- '\x1b[35mw\x1b[0m | \x1b[35ma\x1b[0m | \x1b[35ms\x1b[0m | \x1b[35md\x1b[0m' to step\n";
-    std::cout << "\t\t\t- '\x1b[35mi\x1b[0m | \x1b[35mj\x1b[0m | \x1b[35mk\x1b[0m | \x1b[35ml\x1b[0m' to fire a bullet\n";
+    #if _WIN32
+        std::cout << "\t\t\t- '\x1b[35m↑\x1b[0m | \x1b[35m←\x1b[0m | \x1b[35m↓\x1b[0m | \x1b[35m→\x1b[0m' to move\n";
+    #elif __linux__
+        std::cout << "\t\t\t- '\x1b[35mi\x1b[0m | \x1b[35mj\x1b[0m | \x1b[35mk\x1b[0m | \x1b[35ml\x1b[0m' to fire a bullet\n";
+    #endif
     std::cout << "\t\t\t- '\x1b[35mx\x1b[0m' to enable/disable auto-strike\n";
     std::cout << "\t\t\t- '\x1b[35m+\x1b[0m' to enable/disable cross-strike\n";
     std::cout << "\t\t\t- '\x1b[35mb\x1b[0m' to enable/disable build-mode\n";
